85ebbaf0153da1d6f0f23d5cdfd4988b0b9c199c
Akrwt-dev <volcanoak1@gmail.com>
2026-04-03 16:29:12 +0530

    final changes

--- src/config/database.js
+++ src/config/database.js
@@ -2,7 +2,7 @@ const mongoose = require("mongoose");
 
 const connectDB = async () => {
   await mongoose.connect(
-    "mongodb+srv://volcanoak1_db_user:KEmeeJMATujS5ycx@todo.hzfxmif.mongodb.net/TODO",
+    process.env.MONGO_URI,
   );
 };
 module.exports = connectDB;
